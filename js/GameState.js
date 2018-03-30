bc1a97fb027d5813164a1207ce32c1abc50107c6
Adriano Souza Costa <adrianosouzacostaios@gmail.com>
2018-03-30 15:55:33 -0300

    Emitindo som ao clicar em uma moeda.

--- js/GameState.js
+++ js/GameState.js
@@ -11,16 +11,16 @@ var GameState = {
         this.load.spritesheet('coin', 'assets/images/coin_spritesheet.png', 128, 128, 21);
         
         // Sounds
-        this.load.audio("start", ["assets/audio/start.mp4"]);
-        this.load.audio("end", ["assets/audio/end.mp4"]);
-        this.load.audio("tap", ["assets/audio/tap.mp4"]);
+        this.load.audio("start", ["assets/audio/start.m4a"]);
+        this.load.audio("end", ["assets/audio/end.m4a"]);
+        this.load.audio("tapSound", ["assets/audio/tap.m4a"]);
     },
 
     create: function () {
         var coinsData = [];
 
         for (let index = 0; index < 9; index++) {
-            coinsData.push({ key: "coin", value: 10, position: this.randomPosition() });
+            coinsData.push({ key: "coin", value: 10, position: this.randomPosition(), sound: "tapSound" });
         };
 
         this.coins = this.game.add.group();
@@ -30,7 +30,7 @@ var GameState = {
         coinsData.forEach(function (element) {
             coin = self.coins.create(element.position.x, element.position.y, element.key);
             coin.animations.add('animate', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ,10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20], 24, true);
-            coin.customParams = { value: element.value };
+            coin.customParams = { value: element.value, sound: self.game.add.audio(element.sound) };
             coin.anchor.setTo(0.5);
             coin.scale.setTo(0.2);
             coin.inputEnabled = true;
@@ -45,6 +45,7 @@ var GameState = {
     },
 
     tap: function (coin, event) {
+        coin.customParams.sound.play();
         coin.destroy();
     },
 
